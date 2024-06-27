53c6c33b1fa41f10ea8a2e0817583a59f5499087
oleg-agapov <oleg-agapov@users.noreply.github.com>
2024-06-27 04:09:58 +0000

    Add product info to product searches

--- models/marts/product_searches.sql
+++ models/marts/product_searches.sql
@@ -1,13 +1,45 @@
--- TODO
---- add product info
+with 
+
+product_searches as (
+    select *
+    from {{ ref('stg_rugs_usa_links') }}
+    where base_url = 'https://www.rugsusa.com/rugsusa/control/search-rugs'
+),
+
+products as (
+    select *
+    from {{ ref('stg_rugs_usa_parent') }}
+)
 
 select
-    surrogate_key,
-    base_url,
-    page_links,
-    card_links,
-    product_url,
-    request_time_stamp,
-    dw_insert_timestamp
-from {{ ref('stg_rugs_usa_links') }}
-where base_url = 'https://www.rugsusa.com/rugsusa/control/search-rugs'
+    ps.surrogate_key,
+    ps.base_url,
+    ps.page_links,
+    ps.card_links,
+    ps.product_url,
+    ps.request_time_stamp,
+    ps.dw_insert_timestamp,
+    p.product_id,
+    p.product_type_id,
+    p.product_name,
+    p.origin,
+    p.thickness,
+    p.material,
+    p.weave,
+    p.weave_feature,
+    p.color,
+    p.brand,
+    p.image_name,
+    p.image_type,
+    p.internal_name,
+    p.category,
+    p.min_price,
+    p.max_price,
+    p.availability,
+    p.aggregate,
+    p.clearance,
+    p.long_description,
+    p.shop_by_room
+from product_searches ps
+left join products p 
+    on p.product_url = ps.product_url
